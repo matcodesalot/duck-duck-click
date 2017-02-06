b750ec9f51b4555ef8b9e2ffcace9d67699ef8d0
mat <reallymathew@gmail.com>
2017-02-06 17:59:12 -0600

    add pond

--- client/js/index.js
+++ client/js/index.js
@@ -3,6 +3,12 @@ var quacksPerSecond = 0;
 var autoClick = {
 	price: 50,
 	amount: 0,
+	perSecond: 1,
+};
+var pond = {
+	price: 140,
+	amount: 0,
+	perSecond: 2,
 };
 
 function renderQuacks() {
@@ -20,12 +26,19 @@ function renderAutoClick() {
 	$("#auto-click-amount").text("You own " + autoClick.amount + " auto clickers");
 }
 
+function renderPond() {
+	pond.price = Math.floor(pond.price);
+	$("#pond-price").text(pond.price + " quacks");
+	$("#pond-amount").text("You own " + pond.amount + " ponds");
+}
+
 function renderQuacksPerSecond() {
-	$("quacks-per-second").text("Quacks Per Second: " + quacksPerSecond);
+	$("#quacks-per-second").text("Quacks Per Second: " + quacksPerSecond);
 }
 
 function timer() {
-	quackCount += autoClick.amount;
+	quackCount += autoClick.amount * autoClick.perSecond;
+	quackCount += pond.amount * pond.perSecond;
 	update();
 }
 
@@ -36,8 +49,21 @@ function buyAutoClick() {
 		quackCount -= autoClick.price;
 		autoClick.price = ((autoClick.amount + 1) * 1.2) * autoClick.price;
 		autoClick.amount++;
+		quacksPerSecond += autoClick.perSecond;
 		update();
-		console.log(autoClick.price);
+	}
+	else {
+		console.log("You can't afford");
+	}
+}
+
+function buyPond() {
+	if(quackCount >= pond.price) {
+		quackCount -= pond.price;
+		pond.price = ((pond.amount + 1) * 1.2) * pond.price;
+		pond.amount++;
+		quacksPerSecond += pond.perSecond;
+		update();
 	}
 	else {
 		console.log("You can't afford");
@@ -46,26 +72,35 @@ function buyAutoClick() {
 
 function save() {
 	localStorage.setItem("quackCount", quackCount);
+	localStorage.setItem("quacksPerSecond", quacksPerSecond);
 	localStorage.setItem("autoClickAmount", autoClick.amount);
 	localStorage.setItem("autoClickPrice", autoClick.price);
-	console.log("you saved " + quackCount + " quacks");
+	localStorage.setItem("pondAmount", pond.amount);
+	localStorage.setItem("pondPrice", pond.price);
 }
 
 function load() {
 	quackCount = localStorage.getItem("quackCount");
 	quackCount = parseInt(quackCount);
+	quacksPerSecond = localStorage.getItem("quacksPerSecond");
+	quacksPerSecond = parseInt(quacksPerSecond);
 	autoClick.amount = localStorage.getItem("autoClickAmount");
 	autoClick.amount = parseInt(autoClick.amount);
 	autoClick.price = localStorage.getItem("autoClickPrice");
 	autoClick.price = parseInt(autoClick.price);
+	pond.amount = localStorage.getItem("pondAmount");
+	pond.amount = parseInt(pond.amount);
+	pond.price = localStorage.getItem("pondPrice");
+	pond.price = parseInt(pond.price);
 	update();
-	console.log("you loaded " + quackCount + " quacks");
 }
 
 function update() {
 	renderQuacks();
 	renderTitle();
 	renderAutoClick();
+	renderPond();
+	renderQuacksPerSecond();
 }
 
 $(document).ready(function() {
@@ -78,6 +113,10 @@ $(document).ready(function() {
 	$("#auto-click").click(function() {
 		buyAutoClick();
 	});
+
+	$("#pond").click(function() {
+		buyPond();
+	})
 
 	$("#save").click(function() {
 		save();
